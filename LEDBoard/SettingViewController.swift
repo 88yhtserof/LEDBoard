7f9c4ea058d7bcf808e54d3b798242a7ad3d1dbd
LimYunhwi <88yhtserof@naver.com>
2021-12-03 23:10:39 +0900

    기능 구현하기 1
    1. ViewController와 SettingViewController의 UI 오브젝트들의 아울렛 변수와 액션함수 추가
    2. 컬러 버튼 선택 시 alpha 값 변경되도록 구현

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,69 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by limyunhwi on 2021/12/03.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var btnYellow: UIButton!
+    @IBOutlet weak var btnPurple: UIButton!
+    @IBOutlet weak var btnGreen: UIButton!
+    @IBOutlet weak var btnBlack: UIButton!
+    @IBOutlet weak var btnBlue: UIButton!
+    @IBOutlet weak var btnOrange: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //선택된 버튼의 alpha값을 1로, 선택되지 않은 버튼의 alpha 값을 0.2로 설정한다.
+    @IBAction func tapTextColorButton(_ sender: UIButton) {
+        //한 액션함수에 여러 버튼을 설정할 경우 sender 파라미터를 통해 어떤 버튼이 tap되었는지 알 수 있다.
+        //버튼이 tap된 경우 sender 파라미터에 해당 버튼이 인스턴스로 전달되기 때문에 어떤 버튼이 선택되었는지 알 수 있다.
+        
+        if sender == self.btnYellow {
+            self.changeTextColor(color: .yellow)
+        }else if sender == self.btnPurple {
+            self.changeTextColor(color: .purple)
+        }else {
+            self.changeTextColor(color: .green)
+        }
+    }
+    
+    @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
+        //한 액션함수에 여러 버튼을 설정할 경우 sender 파라미터를 통해 어떤 버튼이 tap되었는지 알 수 있다.
+        //버튼이 tap된 경우 sender 파라미터에 해당 버튼이 인스턴스로 전달되기 때문에 어떤 버튼이 선택되었는지 알 수 있다.
+        
+        if sender == self.btnBlack {
+            self.changeBackgroundColor(color: .black)
+        }else if sender == self.btnOrange {
+            self.changeBackgroundColor(color: .orange)
+        }else {
+            self.changeBackgroundColor(color: .blue)
+        }
+    }
+    
+    @IBAction func tapSaveButton(_ sender: UIButton) {
+    }
+    
+    private func changeTextColor(color: UIColor) {
+        //삼항 연산자 사용
+        self.btnYellow.alpha = color == UIColor.yellow ? 1 : 0.2
+        self.btnPurple.alpha = color == UIColor.purple ? 1 : 0.2
+        self.btnGreen.alpha = color == UIColor.green ? 1 : 0.2
+    }
+    
+    private func changeBackgroundColor(color: UIColor) {
+        //삼항 연산자 사용
+        self.btnBlue.alpha = color == UIColor.blue ? 1 : 0.2
+        self.btnOrange.alpha = color == UIColor.orange ? 1 : 0.2
+        self.btnBlack.alpha = color == UIColor.black ? 1 : 0.2
+    }
+}
--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblContents: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.lblContents.textColor = .yellow
     }
 
 
